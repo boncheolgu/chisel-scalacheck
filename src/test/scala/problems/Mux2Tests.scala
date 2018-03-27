2266516070df3409b04b68dcb50298dc3eab0654
Boncheol Gu <boncheol.gu@gmail.com>
2018-03-27 22:35:22 +0900

    Add labeling options.

--- src/test/scala/problems/Mux2Tests.scala
+++ src/test/scala/problems/Mux2Tests.scala
@@ -4,27 +4,34 @@ package problems
 import chisel3.iotesters.{Driver, PeekPokeTester}
 
 import org.scalacheck.Gen
-import org.scalacheck.Prop.forAll
+import org.scalacheck.Prop.{forAll, BooleanOperators}
 
-import org.scalatest.FreeSpec
+import org.scalatest.{FreeSpec, Matchers}
 
-class Mux2Tester(c: Mux2) extends PeekPokeTester(c) {
+class Mux2Tests extends FreeSpec with Matchers {
   val sel_gen: Gen[Int] = Gen.choose(0, 1)
   val in_gen: Gen[Int] = Gen.choose(0, 1)
+  val options: Array[String] = Array()
 
-  forAll(sel_gen, in_gen, in_gen) {
-    (s: Int, i0: Int, i1: Int) =>
-    poke(c.io.sel, s)
-    poke(c.io.in1, i1)
-    poke(c.io.in0, i0)
-    step(1)
-    expect(c.io.out, if (s == 1) i1 else i0)
-  }.check
-}
-
-class Mux2Tests extends FreeSpec {
   "Mux2Spec" in {
-    Driver.execute(Array(), () => new Mux2) (new Mux2Tester(_))
-    // Driver.execute(Array("--backend-name", "verilator"), () => new Mux2)(new Mux2Tester(_))
+    Driver.execute(options, () => new Mux2) (c => new PeekPokeTester(c) {
+      forAll(sel_gen, in_gen, in_gen) {
+        (s: Int, i0: Int, i1: Int) =>
+        poke(c.io.sel, s)
+        poke(c.io.in1, i1)
+        poke(c.io.in0, i0)
+        step(1)
+        expect(c.io.out, if (s == 1) i1 else i1) :| "labeling option #1"
+      }.check
+
+      forAll(sel_gen, in_gen, in_gen) {
+        (s: Int, i0: Int, i1: Int) =>
+        poke(c.io.sel, s)
+        poke(c.io.in1, i1)
+        poke(c.io.in0, i0)
+        step(1)
+        expect(c.io.out, if (s == 1) i1 else i1, "labeling option #2")
+      }.check
+    })
   }
 }
