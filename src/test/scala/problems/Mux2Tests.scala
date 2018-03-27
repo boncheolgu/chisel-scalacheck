a3f3f5f26b7e30528202b43e89d9703bc13f52d2
Boncheol Gu <boncheol.gu@gmail.com>
2018-03-27 12:52:52 +0900

    Refactor Mux2Tests

--- src/test/scala/problems/Mux2Tests.scala
+++ src/test/scala/problems/Mux2Tests.scala
@@ -1,15 +1,24 @@
 // See LICENSE.txt for license details.
 package problems
 
-import chisel3._
-import chisel3.util._
 import chisel3.iotesters.{Driver, TesterOptionsManager, PeekPokeTester}
 
-import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.Gen
-import org.scalacheck.Properties
 import org.scalacheck.Prop.forAll
-import org.scalacheck.Test
+
+class Mux2Tester(c: Mux2) extends PeekPokeTester(c) {
+  val sel_gen: Gen[Int] = Gen.choose(0, 1)
+  val in_gen: Gen[Int] = Gen.choose(0, 1)
+
+  forAll(sel_gen, in_gen, in_gen) {
+    (s: Int, i0: Int, i1: Int) =>
+    poke(c.io.sel, s)
+    poke(c.io.in1, i1)
+    poke(c.io.in0, i0)
+    step(1)
+    expect(c.io.out, if (s == 1) i1 else i0)
+  }.check
+}
 
 object Mux2Tests {
   def main(args: Array[String]): Unit = {
@@ -17,22 +26,6 @@ object Mux2Tests {
     optionsManager.doNotExitOnHelp()
     optionsManager.parse(args)
 
-    val sel_gen: Gen[Int] = Gen.choose(0, 1)
-    val in_gen: Gen[Int] = Gen.choose(0, 1)
-
-    Driver.execute(() => new Mux2, optionsManager) { (c) =>
-      new PeekPokeTester(c) {
-        val prop =
-          forAll(sel_gen, in_gen, in_gen)((s: Int, i0: Int, i1: Int) => {
-            poke(c.io.sel, s)
-            poke(c.io.in1, i1)
-            poke(c.io.in0, i0)
-            step(1)
-            expect(c.io.out, if (s == 1) i1 else i0)
-          })
-
-        prop.check
-      }
-    }
+    Driver.execute(() => new Mux2, optionsManager)((c) => new Mux2Tester(c))
   }
 }
